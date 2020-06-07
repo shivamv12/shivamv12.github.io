73a6838dbc1b9b5b511d15cffc8d1a7d03f0e6c3
shivamv12 <shivambca75@gmail.com>
2020-06-07 18:03:58 +0530

    Updated Skills, Portfolio & References

--- js/main.js
+++ js/main.js
@@ -5,136 +5,128 @@
  ------------------------------------ 
  ====================================*/
 
-
-
 'use strict';
 
-
-$(window).on('load', function() { 
-	/*------------------
+$(window).on('load', function () {
+  /*------------------
 		Preloder
 	--------------------*/
-	$(".loader").fadeOut(); 
-	$("#preloder").delay(400).fadeOut("slow");
-
+  $('.loader').fadeOut();
+  $('#preloder').delay(400).fadeOut('slow');
 });
 
-
-(function($) {
-
-	/*------------------
+(function ($) {
+  /*------------------
 		Background set
 	--------------------*/
-	$('.set-bg').each(function() {
-		var bg = $(this).data('setbg');
-		$(this).css('background-image', 'url(' + bg + ')');
-	});
+  $('.set-bg').each(function () {
+    var bg = $(this).data('setbg');
+    $(this).css('background-image', 'url(' + bg + ')');
+  });
 
+  $('.review-slider').owlCarousel({
+    loop: true,
+    nav: false,
+    dots: true,
+    items: 1,
+    autoplay: true,
+  });
 
-	$('.review-slider').owlCarousel({
-		loop: true,
-		nav: false,
-		dots: true,
-		items: 1,
-		autoplay: true
-	});
+  $('.progress-bar-style').each(function () {
+    var progress = $(this).data('progress');
+    var prog_width = progress + '%';
+    if (progress <= 100) {
+      $(this).append(
+        '<div class="bar-inner" style="width:' +
+          prog_width +
+          '"><span>' +
+          prog_width +
+          '</span></div>'
+      );
+    } else {
+      $(this).append(
+        '<div class="bar-inner" style="width:100%"><span>' +
+          prog_width +
+          '</span></div>'
+      );
+    }
+  });
 
+  $('.lan-prog').each(function () {
+    var progress = $(this).data('lanprogesss');
+    var ele = '<span></span>';
+    var ele_fade = '<span class="fade-ele"></span>';
 
+    for (var i = 1; i <= 5; i++) {
+      if (i <= progress) {
+        $(this).append(ele);
+      } else {
+        $(this).append(ele_fade);
+      }
+    }
+  });
 
-	$('.progress-bar-style').each(function() {
-		var progress = $(this).data("progress");
-		var prog_width = progress + '%';
-		if (progress <= 100) {
-			$(this).append('<div class="bar-inner" style="width:' + prog_width + '"><span>' + prog_width + '</span></div>');
-		}
-		else {
-			$(this).append('<div class="bar-inner" style="width:100%"><span>' + prog_width + '</span></div>');
-		}
-	});
-
-
-	$('.lan-prog').each(function() {
-		var progress = $(this).data("lanprogesss");
-		var ele      = '<span></span>';
-		var ele_fade = '<span class="fade-ele"></span>';
-		
-		for (var i = 1; i <= 5; i++) {
-			if(i <= progress){
-				$(this).append(ele);
-			} else {
-				$(this).append(ele_fade);
-			}
-		}
-	});
-
-
-	/*------------------
+  /*------------------
 		Popup
 	--------------------*/
-	$('.portfolio-item .port-pic').magnificPopup({
-		type: 'image',
-		mainClass: 'img-popup-warp',
-		removalDelay: 500,
-	});
+  $('.portfolio-item .port-pic').magnificPopup({
+    type: 'image',
+    mainClass: 'img-popup-warp',
+    removalDelay: 500,
+  });
 
+  if ($().circleProgress) {
+    //Set progress circle 1
+    $('#progress1').circleProgress({
+      value: 0.6,
+      size: 175,
+      thickness: 2,
+      fill: '#40424a',
+      emptyFill: 'rgba(0, 0, 0, 0)',
+    });
+    //Set progress circle 2
+    $('#progress2').circleProgress({
+      value: 0.5,
+      size: 175,
+      thickness: 2,
+      fill: '#40424a',
+      emptyFill: 'rgba(0, 0, 0, 0)',
+    });
 
+    //Set progress circle white
+    $('#progress3').circleProgress({
+      value: 0.30,
+      size: 175,
+      thickness: 2,
+      fill: '#40424a',
+      emptyFill: 'rgba(0, 0, 0, 0)',
+    });
 
+    //Set progress circle white
+    $('#progress4').circleProgress({
+      value: 0.83,
+      size: 175,
+      thickness: 2,
+      fill: '#ffffff',
+      emptyFill: 'rgba(0, 0, 0, 0)',
+    });
 
-if($().circleProgress){
+    //Set progress circle skyblue
+    $('#progress5').circleProgress({
+      value: 0.75,
+      size: 175,
+      thickness: 2,
+      fill: '#009fff',
+      emptyFill: 'rgba(0, 0, 0, 0)',
+    });
 
-	//Set progress circle 1
-	$("#progress1").circleProgress({
-		value: 0.75,
-		size: 175,
-		thickness: 2,
-		fill: "#40424a",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-	//Set progress circle 2
-	$("#progress2").circleProgress({
-		value: 0.83,
-		size: 175,
-		thickness: 2,
-		fill: "#40424a",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-
-	//Set progress circle white
-	$("#progress3").circleProgress({
-		value: 0.75,
-		size: 175,
-		thickness: 2,
-		fill: "#ffffff",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-
-	//Set progress circle white
-	$("#progress4").circleProgress({
-		value: 0.83,
-		size: 175,
-		thickness: 2,
-		fill: "#ffffff",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-
-	//Set progress circle skyblue
-	$("#progress5").circleProgress({
-		value: 0.75,
-		size: 175,
-		thickness: 2,
-		fill: "#009fff",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-
-	//Set progress circle skyblue
-	$("#progress6").circleProgress({
-		value: 0.83,
-		size: 175,
-		thickness: 2,
-		fill: "#009fff",
-		emptyFill: "rgba(0, 0, 0, 0)"
-	});
-}
-
+    //Set progress circle skyblue
+    $('#progress6').circleProgress({
+      value: 0.83,
+      size: 175,
+      thickness: 2,
+      fill: '#009fff',
+      emptyFill: 'rgba(0, 0, 0, 0)',
+    });
+  }
 })(jQuery);
-
